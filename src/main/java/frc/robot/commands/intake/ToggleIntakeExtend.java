fa87fed26916721da5cadc9ad24de19e84cce359
Henry <yehenry11@gmail.com>
2020-02-19 17:45:12 -0500

    Not retracting in end() for ToggleIntakeExtend

--- src/main/java/frc/robot/commands/intake/ToggleIntakeExtend.java
+++ src/main/java/frc/robot/commands/intake/ToggleIntakeExtend.java
@@ -41,7 +41,6 @@ public class ToggleIntakeExtend extends CommandBase {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-        m_intake.retract();
     }
 
     // Returns true when the command should end.
