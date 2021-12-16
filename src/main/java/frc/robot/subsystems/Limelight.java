2b92992c1f99a4aed0d8325ca3bde0b38cf8671e
cubicalCroissant <94579422+cubicalCroissant@users.noreply.github.com>
2021-12-16 22:12:40 +0900

    Added accessor methods and some functionality(?)

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -2,9 +2,12 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+
+import static frc.robot.Constants.Limelight.*;
 
 public class Limelight extends SubsystemBase {
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
@@ -31,25 +34,25 @@ public class Limelight extends SubsystemBase {
     NetworkTableEntry snapshot = table.getEntry("snapshot");
 
 
-    public static class PeriodicIO { //cursed Java struct equivalent w/ an excessive amount of boilerplate code
-        //outputs from limelight
-        public int ledMode = 0; //0: pipeline default, 1: force off, 2: force blink, 3: force on
-        public int camMode = 0; //0: vison processing, 1: driver camera (increases exposure, disables vision processing)
-        public int pipeline = 0; //0-9; sets limelight pipeline
+    public static class PeriodicIO { //cursed Java struct equivalent
+        //inputs to limelight
+        public int setLedMode = 0; //0: pipeline default, 1: force off, 2: force blink, 3: force on
+        public int setCamMode = 0; //0: vison processing, 1: driver camera (increases exposure, disables vision processing)
+        public int setPipeline = 0; //0-9; sets limelight pipeline
         public int stream = 1; //0: side-by-side streams for webcam attached to limelight, 1: secondary camera in lower right of primary camera stream; 2: reverse of 1
         public int snapshot = 0; //0: no snapshots; 1: 2 snapshots per second
 
-        //inputs to limelight
+        //outputs from limelight
         public boolean hasTarget;
         public double x, y, area, skew, latency;
-        public int width, length, horizontal, vertical, setLedMode, setPipeline;
+        public int width, length, horizontal, vertical, ledMode, pipeline;
     }
 
-    private void updateConstants(){
+    private void updateConstants() {
         if(mPeriodicIO.setLedMode != mPeriodicIO.ledMode || mPeriodicIO.setPipeline != mPeriodicIO.pipeline){
-            ledMode.setNumber(mPeriodicIO.ledMode);
-            camMode.setNumber(mPeriodicIO.camMode);
-            pipeline.setNumber(mPeriodicIO.pipeline);
+            ledMode.setNumber(mPeriodicIO.setLedMode);
+            camMode.setNumber(mPeriodicIO.setCamMode);
+            pipeline.setNumber(mPeriodicIO.setPipeline);
             stream.setNumber(mPeriodicIO.stream);
             snapshot.setNumber(mPeriodicIO.snapshot);
         }
@@ -57,8 +60,36 @@ public class Limelight extends SubsystemBase {
 
     private PeriodicIO mPeriodicIO = new PeriodicIO();
 
+    public void setPipeline(int _pipe){
+        mPeriodicIO.setPipeline = _pipe;
+    }
+
+    public void setLedMode(int _ledMode) {
+        mPeriodicIO.setLedMode = _ledMode;
+    }
+
+    public boolean hasTarget(){
+        return mPeriodicIO.hasTarget;
+    }
+
+    public double[] getTargetOffset() {
+        return new double[] {mPeriodicIO.x, mPeriodicIO.y, };
+    }
+
+    public double getDistance() {
+        return((targetHeight - robotHeight)/Math.tan(mPeriodicIO.y + offsetAngle));
+    }
+
+    public double getArea() {
+        return mPeriodicIO.area;
+    }
+
+    public double[] getDimensions() {
+        return new double[] {mPeriodicIO.horizontal, mPeriodicIO.vertical};
+    }
+
     public double getLatency() {
-        return mPeriodicIO.latency;
+        return(mPeriodicIO.latency + 11);
     }
 
     @Override
@@ -66,11 +97,11 @@ public class Limelight extends SubsystemBase {
         updateConstants();
 
         mPeriodicIO.hasTarget = tv.getDouble(0) == 1.0; //Number of valid targets; 0 or 1 converted to false or true
-        mPeriodicIO.x = tx.getDouble(0.0); //Horizontal offset from crosshair to target; ±27° in LL1, ±29.8° in LL2 
-        mPeriodicIO.y = ty.getDouble(0.0); //Vertical offset from crosshair to target; ±20.5° in LL1, ±24.85° in LL2
+        mPeriodicIO.x = tx.getDouble(0.0); //Horizontal offset from crosshair to target; ±29.8° in LL2 
+        mPeriodicIO.y = ty.getDouble(0.0); //Vertical offset from crosshair to target; ±24.85° in LL2
         mPeriodicIO.area = ta.getDouble(0.0); //Target area; 0% to 100%
         mPeriodicIO.skew = ts.getDouble(0.0); //Skew or rotation of the target, from -90 degrees to 0 degrees
-        mPeriodicIO.latency = tl.getDouble(0.0) + 11; //In ms, add >= 11 ms for image capture latency
+        mPeriodicIO.latency = tl.getDouble(0.0); //In ms, add >= 11 ms for image capture latency
         mPeriodicIO.width = (int) tshort.getDouble(0); //Length of shortest side of bounding box, in pixels
         mPeriodicIO.length = (int) tlong.getDouble(0); //Length of longest side of bounding box, in pixels
         mPeriodicIO.horizontal = (int) thor.getDouble(0); //Length of horizontal side of bounding box, 0-320 pixels
@@ -83,6 +114,6 @@ public class Limelight extends SubsystemBase {
         SmartDashboard.putNumber("LimelightArea", mPeriodicIO.area);
         SmartDashboard.putNumber("LimelightSkew", mPeriodicIO.skew);
         SmartDashboard.putNumber("LimelightLatency", mPeriodicIO.latency);
-        SmartDashboard.putNumber("LimelightPipeline", mPeriodicIO.setPipeline);
+        SmartDashboard.putNumber("LimelightPipeline", mPeriodicIO.pipeline);
     }
 }
