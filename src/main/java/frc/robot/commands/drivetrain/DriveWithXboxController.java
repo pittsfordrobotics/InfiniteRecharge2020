ae67356df265d4c21c4bdbeffa38af4a6d760706
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2022-01-20 23:28:54 -0500

    added maybe only accelerate when slowing down

--- src/main/java/frc/robot/commands/drivetrain/DriveWithXboxController.java
+++ src/main/java/frc/robot/commands/drivetrain/DriveWithXboxController.java
@@ -8,7 +8,6 @@
 package frc.robot.commands.drivetrain;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
@@ -16,10 +15,8 @@ import frc.robot.subsystems.DriveTrain;
 public class DriveWithXboxController extends CommandBase {
     private DriveTrain m_driveTrain;
     private XboxController m_controller;
-    private double throttle;
-    private double limitedThrottle;
-    private double offset;
-    private Timer m_timer = new Timer();
+    private double pastInput;
+    private boolean accelerate;
 
     /**
      * Creates a new DriveWithJoysticks.
@@ -33,17 +30,26 @@ public class DriveWithXboxController extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-        m_timer.reset();
-        m_timer.start();
-        offset = -m_controller.getY(Hand.kLeft);
-        throttle = 0;
-        limitedThrottle = 0;
+        accelerate = false;
+        pastInput = 0;
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        m_driveTrain.drive(m_driveTrain.getRateLimit().calculate(-m_controller.getY(Hand.kLeft)), m_controller.getX(Hand.kRight) * 0.75);
+        if (accelerate && Math.abs(m_driveTrain.getLeftVelocity()) > 0) {
+            accelerate = true;
+        }
+        else {
+            accelerate = -m_controller.getY(Hand.kLeft) - pastInput < 0;
+        }
+        pastInput = -m_controller.getY(Hand.kLeft);
+        if (accelerate) {
+            m_driveTrain.drive(m_driveTrain.getRateLimit().calculate(-m_controller.getY(Hand.kLeft)), m_controller.getX(Hand.kRight) * 0.75);
+        }
+        else {
+            m_driveTrain.drive(-m_controller.getY(Hand.kLeft), m_controller.getX(Hand.kRight) * 0.75);
+        }
     }
 
     // Called once the command ends or is interrupted.
